ffa85189008a25056a019578d5e5c2f57322ad6a
Omar Flores <sazardev@gmail.com>
2024-06-24 11:45:03 -0700

    chore: Add ThemeProvider component for managing theme context

---
+++ src/config/theme/ThemeProvider.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ThemeProvider({ children }: Props) {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+}
