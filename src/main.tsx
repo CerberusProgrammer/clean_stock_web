1c9497a95417ff62230d49eda0f954affdcdfb06
Omar Flores <sazardev@gmail.com>
2024-06-24 11:45:06 -0700

    feat: Add ThemeProvider and RouterProvider components for managing theme and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ThemeProvider from "./config/theme/ThemeProvider.tsx";
+import { RouterProvider } from "react-router-dom";
+import { ROUTES } from "./config/router/router.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <RouterProvider router={ROUTES} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
